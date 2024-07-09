d81bdb2cddcb326971648b03d8901b22e8f86adb
cocoquiet <cocoquiet@knu.ac.kr>
2024-07-09 07:14:40 +0000

    Fix: Change env type to int

--- cogs/owner.py
+++ cogs/owner.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 def has_permissions_or_owner():
     async def predicate(ctx):
-        if ctx.author.id == os.getenv('OWNER_ID') or ctx.author.guild_permissions.administrator:
+        if ctx.author.id == int(os.getenv('OWNER_ID')) or ctx.author.guild_permissions.administrator:
             return True
         else:
             raise commands.CheckFailure()
